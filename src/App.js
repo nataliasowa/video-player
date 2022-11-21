41eb7b92cc31a8f1bdf27990a5991273fc88fb6a
Natalia Sowa <natalia.sowaa@gmail.com>
2022-11-21 18:15:28 +0100

    added video progress functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import PlayerButton from "./components/PlayerButton";
 import MuteButton from "./components/MuteButton";
 import SpeedButton from "./components/SpeedButton";
 import FullscreenButton from "./components/FullscreenButton";
+import VideoProgressBar from "./components/VideoProgressBar";
+import MouseMoveControls from "./components/MouseMoveControls";
 import { FaPlay } from "react-icons/fa";
 import { FaPause } from "react-icons/fa";
 import { FaVolumeMute } from "react-icons/fa";
@@ -20,70 +22,85 @@ function App() {
   const { speed, handleVideoSpeed } = SpeedButton(videoRef);
   const { isFullscreen, makeFullscreen, closeFullscreen } =
     FullscreenButton(videoRef);
+  const { progress, handleVideoProgress, handleOnTimeUpdate } =
+    VideoProgressBar(videoRef);
+  const { showControls } = MouseMoveControls(videoRef);
   return (
     <div className="container">
       <div className="video-wrapper">
-        <video src={video} ref={videoRef} autoPlay />
-        <div className="controls">
-          {/* TIMELINE */}
-          <div>
-            <input
-              type="range"
-              min="0"
-              max="100"
-              step={0.001}
-              class="video-progress-bar"
-            />
-          </div>
-          <div className="actions">
-            {/* PlAY AND MUTE THE VIDEO WITH VOLUME RANGE */}
+        <video
+          src={video}
+          ref={videoRef}
+          onTimeUpdate={handleOnTimeUpdate}
+          autoPlay
+        />
+        {showControls && (
+          <div className="controls">
+            {/* TIMELINE */}
             <div>
-              <button onClick={togglePlay} className="play-pause-btn">
-                {isPlaying ? <FaPause size={25} /> : <FaPlay size={25} />}
-              </button>
-              <button onClick={toggleMute} className="mute-unmute-btn">
-                {isMuted ? <FaVolumeMute size={25} /> : <GoUnmute size={25} />}
-              </button>
               <input
                 type="range"
-                onChange={changeVideoVolume}
-                class="volume-bar"
-                id="volume"
+                onChange={(e) => handleVideoProgress(e)}
                 min="0"
-                max="1"
-                step={0.1}
-                value={isMuted ? 0 : volume}
-                disabled={isMuted}
+                max="100"
+                step={0.001}
+                value={progress}
+                class="video-progress-bar"
               />
             </div>
-            <div>
-              <p>Title</p>
-            </div>
-            {/* SPEED AND FULLSCREEN THE VIDEO */}
-            <div>
-              <select
-                className="velocity"
-                value={speed}
-                onChange={handleVideoSpeed}
-              >
-                <option value="0.50">0.50x</option>
-                <option value="0.1">1x</option>
-                <option value="1.25">1.25x</option>
-                <option value="2">2x</option>
-              </select>
-              <button
-                onClick={isFullscreen ? closeFullscreen : makeFullscreen}
-                className="fullscreen-btn"
-              >
-                {isFullscreen ? (
-                  <FaCompress size={25} />
-                ) : (
-                  <FaExpand size={25} />
-                )}
-              </button>
+            <div className="actions">
+              {/* PlAY AND MUTE THE VIDEO WITH VOLUME RANGE */}
+              <div>
+                <button onClick={togglePlay} className="play-pause-btn">
+                  {isPlaying ? <FaPause size={25} /> : <FaPlay size={25} />}
+                </button>
+                <button onClick={toggleMute} className="mute-unmute-btn">
+                  {isMuted ? (
+                    <FaVolumeMute size={25} />
+                  ) : (
+                    <GoUnmute size={25} />
+                  )}
+                </button>
+                <input
+                  type="range"
+                  onChange={changeVideoVolume}
+                  class="volume-bar"
+                  id="volume"
+                  min="0"
+                  max="1"
+                  step={0.1}
+                  value={isMuted ? 0 : volume}
+                  disabled={isMuted}
+                />
+              </div>
+              <div>
+                <p>Title</p>
+              </div>
+              {/* SPEED AND FULLSCREEN THE VIDEO */}
+              <div>
+                <select
+                  className="velocity"
+                  value={speed}
+                  onChange={handleVideoSpeed}
+                >
+                  <option value="0.1">1x</option>
+                  <option value="1.25">1.25x</option>
+                  <option value="2">2x</option>
+                </select>
+                <button
+                  onClick={isFullscreen ? closeFullscreen : makeFullscreen}
+                  className="fullscreen-btn"
+                >
+                  {isFullscreen ? (
+                    <FaCompress size={25} />
+                  ) : (
+                    <FaExpand size={25} />
+                  )}
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
